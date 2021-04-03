801b53e7f8b443a46933ffd62b4399a03c9d8706
Nevena <nevenablagojevic95@gmail.com>
2021-04-03 16:37:05 +0200

    map html

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
     <section class="topBar container">
         <a href="" class="logo">Low <span>Office</span></a>
         <ul>
-        <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
-        <li><a href=""><i class="fab fa-twitter"></i></a></li>
-        <li><a href=""><i class="fab fa-instagram"></i></a></li>
+            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li><a href=""><i class="fab fa-instagram"></i></a></li>
         </ul>
     </section>
 
@@ -31,7 +31,10 @@
         </nav>
         <hgroup>
             <h1>Experience - Trust - Results</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, ea. Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati? Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, ea. Fugit ab nihil eveniet accusamus
+                vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea
+                similique labore obcaecati? Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos
+                recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati?</p>
             <button>view more</button>
         </hgroup>
     </header>
@@ -55,5 +58,14 @@
         </article>
     </section>
 
+    <!--Map-->
+    <section class="map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321202.81351507094!2d-114.36801505489849!3d51.02728832742343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2z0JrQsNC70LPQsNGA0LgsINCQ0LvQsdC10YDRgtCwLCDQmtCw0L3QsNC00LA!5e0!3m2!1ssr!2srs!4v1617460499619!5m2!1ssr!2srs"
+            width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy">
+        </iframe>
+    </section>
+
 </body>
+
 </html>
