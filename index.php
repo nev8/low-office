33c3305ba3f6733919aff20e94d08c0a1b46ca42
Nevena <nevenablagojevic95@gmail.com>
2021-04-03 14:47:45 +0200

    topbar html and varijable css

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Law office</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="scss/style.css">
+
+<body>
+    <!--Top Bar-->
+    <section class="topBar">
+        <a href="" class="logo">Low <span>office</span></a>
+        <ul>
+        <li><a href=""><i class="fab fa-facebook"></i></a></li>
+        <li><a href=""><i class="fab fa-twitter"></i></a></li>
+        <li><a href=""><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </section>
+
+</body>
+
+</html>
