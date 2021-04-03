ebf465bf5b3b892efc29d4d67985a83b5b32c331
Nevena <nevenablagojevic95@gmail.com>
2021-04-03 22:37:04 +0200

    izmena strukture peoples sekcije

--- index.php
+++ index.php
@@ -2,37 +2,47 @@
 
 <!--Services-->
 <section class="services container">
-     <article>
-         <i class="fas fa-landmark"></i>
-         <h3>Family low</h3>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-         <button>Learn more</button>
-     </article>
-     <article>
-         <i class="fas fa-landmark"></i>
-         <h3>Business law</h3>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-         <button>Learn more</button>
-     </article>
-     <article>
-         <i class="fas fa-landmark"></i>
-         <h3>Criminal law</h3>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-         <button>Learn more</button>
-     </article>
-     <article>
-         <i class="fas fa-landmark"></i>
-         <h3>Insurance law</h3>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-         <button>Learn more</button>
-     </article>
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Family low</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Business law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Criminal law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Insurance law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
 </section>
 
 <!--About-->
 <section class="about">
     <article>
         <h2>About us</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vel cum nulla mollitia earum ad maxime laudantium ipsa cupiditate totam nostrum vero, reiciendis consequatur sunt voluptates laborum, ducimus repellendus possimus at autem deleniti! Facilis est vero cum consequuntur aliquam nobis!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vel cum nulla mollitia earum ad maxime
+            laudantium ipsa cupiditate totam nostrum vero, reiciendis consequatur sunt voluptates laborum, ducimus
+            repellendus possimus at autem deleniti! Facilis est vero cum consequuntur aliquam nobis!</p>
         <a href="">Learn more &nbsp; &rang;</a>
     </article>
     <article>
@@ -43,43 +53,59 @@
 <!--Team-->
 <section class="team">
     <h2>Our team</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime animi eaque sed esse eum eius consectetur neque praesentium voluptatum? Cupiditate, ex, ratione. Delectus fuga in iste assumenda ipsum perspiciatis? Magni adipisci pariatur eveniet explicabo velit dolores sunt aspernatur ipsam dolorum nihil facere veritatis, aperiam delectus id rem vitae fugiat iste laboriosam similique amet, ratione quasi. Tempora magni soluta temporibus pariatur voluptatibus natus iusto, repellat ratione qui, assumenda, recusandae quos id! Ea impedit, culpa optio aperiam, beatae quibusdam ex deleniti minima.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime animi eaque sed esse eum eius consectetur neque
+        praesentium voluptatum? Cupiditate, ex, ratione. Delectus fuga in iste assumenda ipsum perspiciatis? Magni
+        adipisci pariatur eveniet explicabo velit dolores sunt aspernatur ipsam dolorum nihil facere veritatis, aperiam
+        delectus id rem vitae fugiat iste laboriosam similique amet, ratione quasi. Tempora magni soluta temporibus
+        pariatur voluptatibus natus iusto, repellat ratione qui, assumenda, recusandae quos id! Ea impedit, culpa optio
+        aperiam, beatae quibusdam ex deleniti minima.</p>
 </section>
 
 <!--Peoples-->
 <section class="peoples">
-    <article class="container">
-        <div>
+    <div class="container">
+        <article>
             <img src="img/people1.jpg" alt="">
-            <h3>Etta James</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
-            <button>Learn more</button>
-            <button>Contact</button>
-        </div>
-        <div>
+            <div class="text">
+                <h3>Etta James</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
             <img src="img/people2.jpg" alt="">
-            <h3>Louis Amstrong</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
-            <button>Learn more</button>
-            <button>Contact</button>
-        </div>
-        <div>
+            <div class="text">
+                <h3>Louis Amstrong</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
             <img src="img/people3.jpg" alt="">
-            <h3>Etta James</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
-            <button>Learn more</button>
-            <button>Contact</button>
-        </div>
-        <div>
+            <div class="text">
+                <h3>Etta James</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
             <img src="img/people4.jpg" alt="">
-            <h3>Louis Amstrong</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
-            <button>Learn more</button>
-            <button>Contact</button>
-        </div>
-    </article>
+            <div class="text">
+                <h3>Louis Amstrong</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum
+                    alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+    </div>
 </section>
 
 <?php include "partials/footer.php"; ?>
-
-    
