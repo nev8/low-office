7ab2db0c23db1d5455d1554c355b2d38f7144b9b
Nevena <nevenablagojevic95@gmail.com>
2021-04-03 15:31:22 +0200

    topBar css

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Law office</title>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="scss/style.css">
+    <link rel="stylesheet" href="style.css">
 
 <body>
     <!--Top Bar-->
-    <section class="topBar">
+    <section class="topBar container">
         <a href="" class="logo">Low <span>Office</span></a>
         <ul>
         <li><a href=""><i class="fab fa-facebook"></i></a></li>
